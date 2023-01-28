cde0ff78f87727d89b73fada5ac53b77b4607c28
Michael36500 <ambros.michael@seznam.cz>
2023-01-28 21:50:07 +0100

    před vyškrtnutí rgh senzoru

--- doubleidoto sumo inteligente.py
+++ doubleidoto sumo inteligente.py
@@ -12,14 +12,21 @@ pravy_motor_navic = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
 levy_motor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 levy_motor_navic = Motor(Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
 # a senzůrů
-left_u = UltrasonicSensor(Port.S4)
+# left_u = UltrasonicSensor(Port.S4)
 right_u = UltrasonicSensor(Port.S1)
 
 
 # nastaví čekací hodnotu
 vyladeno = 0.35
 
+def brut_min(prva, druha, o_kolik):
+    if prva + o_kolik > druha:      return "lft"
+    elif druha + o_kolik > prva:    return "rgh"
+    else:                           return "nic"
+
+
 # čekám na zmáčknutí tlačítka
+ev3.light.on(Color.ORANGE)
 while True:
     if ev3.buttons.pressed() != []: # type: ignore
         ev3.light.on(Color.RED)
@@ -27,13 +34,13 @@ while True:
 # čekám
 # time.sleep(4.9)
 
-# otáčím se o 90
-levy_motor.track_target(360*1000)
-levy_motor_navic.track_target(360*1000)
-pravy_motor.track_target(-360*1000)
-pravy_motor_navic.track_target(-360*1000)
-# čekám čekací hodnotu
-time.sleep(vyladeno)
+# # otáčím se o 90
+# levy_motor.track_target(360*1000)
+# levy_motor_navic.track_target(360*1000)
+# pravy_motor.track_target(-360*1000)
+# pravy_motor_navic.track_target(-360*1000)
+# # čekám čekací hodnotu
+# time.sleep(vyladeno)
 
 # jedu rovně
 levy_motor.track_target(360*1000)
@@ -46,66 +53,26 @@ while True:
     lft = left_u.distance()
     rgh = right_u.distance()
     mini = min([lft, rgh])
+    brutus_mini = brut_min(lft, rgh, o_kolik = 100)
+    print(lft, rgh)
 
-    thresh = 500
+    thresh = 300
 
-    if mini > thresh:
-        # otáčej se na místě, hledej
-        # doprava
-        levy_motor.track_target(360*1000)
-        levy_motor_navic.track_target(360*1000)
-        pravy_motor.track_target(-360*1000)
-        pravy_motor_navic.track_target(-360*1000)
-
-
-    if mini < thresh: # type: ignore
-        # jeď dopředu
+    if lft == 2550 or rgh == 2550 or lft < 100 or rgh < 100:
+        print("jsem v protivníkovi")
         levy_motor.track_target(360*1000)
         levy_motor_navic.track_target(360*1000)
         pravy_motor.track_target(360*1000)
         pravy_motor_navic.track_target(360*1000)
 
 
-
-
-
-
-
-
-
-
-
-
-
-# while True:
-#     lft = left_u.distance()
-#     rgh = right_u.distance()
-#     mini = min([lft, rgh])
-#     if mini > 500:
-#         if lft == mini:
-#             levy_motor.brake()
-#             levy_motor_navic.brake()
-#             pravy_motor.track_target(360*1000)
-#             pravy_motor_navic.track_target(360*1000)
-
-#         elif rgh == mini:
-#             pravy_motor.brake()
-#             pravy_motor_navic.brake()
-#             levy_motor.track_target(360*1000)
-#             levy_motor_navic.track_target(360*1000)
-
-#         else:
-#             # jedu rovně
-#             levy_motor.track_target(360*1000)
-#             levy_motor_navic.track_target(360*1000)
-#             pravy_motor.track_target(360*1000)
-#             pravy_motor_navic.track_target(360*1000)
-            
-
-
-
-# time.sleep(1)
-
-# while True:
-#     if ev3.buttons.pressed() != []: # type: ignore
-#         break
+    elif mini > thresh:
+        # otáčej se na místě, hledej
+        # doprava
+        levy_motor.track_target(360*1000)
+        levy_motor_navic.track_target(360*1000)
+        pravy_motor.track_target(-360*1000)
+        pravy_motor_navic.track_target(-360*1000)
+    else:
+        # hledáš protivníka podle lft a rgh
+        pass
