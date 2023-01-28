41cedf5d011b5da4aa115e4becaea307f2f52e17
Michael36500 <ambros.michael@seznam.cz>
2023-01-28 22:02:51 +0100

    voy a dormir. al final por la noche

--- doubleidoto sumo inteligente.py
+++ doubleidoto sumo inteligente.py
@@ -12,17 +12,34 @@ pravy_motor_navic = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE)
 levy_motor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 levy_motor_navic = Motor(Port.D, positive_direction=Direction.COUNTERCLOCKWISE)
 # a senzůrů
-# left_u = UltrasonicSensor(Port.S4)
-right_u = UltrasonicSensor(Port.S1)
+# left_u = UltrasonicSensor(Port.S4) ABY SE NERUŠILI https://forums.firstinspires.org/forum/general-discussions/first-programs/first-lego-league/the-challenge/products-equipment/ev3-specific/15114-do-multiple-ultrasonic-sensors-facing-same-way-interfere
+ultras = UltrasonicSensor(Port.S4)
 
+def min_lft_rgh():
+    # doprava
+    levy_motor.track_target(-360*1000)
+    levy_motor_navic.track_target(-360*1000)
+    pravy_motor.track_target(360*1000)
+    pravy_motor_navic.track_target(360*1000)
+    
+    lft = ultras.distance()
+
+    # doprava
+    levy_motor.track_target(360*1000)
+    levy_motor_navic.track_target(360*1000)
+    pravy_motor.track_target(-360*1000)
+    pravy_motor_navic.track_target(-360*1000)
+    
+    rgh = ultras.distance()
+
+    if lft > rgh: return "left"
+    if lft < rgh: return "right"
+    else: return "stejne"
+
+    
 
 # nastaví čekací hodnotu
 vyladeno = 0.35
-
-def brut_min(prva, druha, o_kolik):
-    if prva + o_kolik > druha:      return "lft"
-    elif druha + o_kolik > prva:    return "rgh"
-    else:                           return "nic"
 
 
 # čekám na zmáčknutí tlačítka
@@ -50,15 +67,12 @@ pravy_motor_navic.track_target(360*1000)
 
 
 while True:
-    lft = left_u.distance()
-    rgh = right_u.distance()
-    mini = min([lft, rgh])
-    brutus_mini = brut_min(lft, rgh, o_kolik = 100)
-    print(lft, rgh)
+    dist = ultras.distance()
+    print(dist)
 
     thresh = 300
 
-    if lft == 2550 or rgh == 2550 or lft < 100 or rgh < 100:
+    if dist < 100:
         print("jsem v protivníkovi")
         levy_motor.track_target(360*1000)
         levy_motor_navic.track_target(360*1000)
@@ -66,7 +80,7 @@ while True:
         pravy_motor_navic.track_target(360*1000)
 
 
-    elif mini > thresh:
+    elif dist > thresh:
         # otáčej se na místě, hledej
         # doprava
         levy_motor.track_target(360*1000)
@@ -75,4 +89,6 @@ while True:
         pravy_motor_navic.track_target(-360*1000)
     else:
         # hledáš protivníka podle lft a rgh
+        min_lft_rgh()
+        print ("fuck y'a all")
         pass
